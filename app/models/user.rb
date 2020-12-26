d6f8f08d263df13d0edefa29be549a46b428e4e4
kenta-oga <kenta555phi@gmail.com>
2020-12-26 21:03:09 +0900

    rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
     validates :first_name
     validates :family_name
@@ -12,10 +12,9 @@ class User < ApplicationRecord
     validates :first_name_kana
     validates :family_name_kana
   end
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-    validates_format_of :password, with: PASSWORD_REGEX
-  
-    validates :nickname, presence: true
-    validates :birth_date, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 
+  validates :nickname, presence: true
+  validates :birth_date, presence: true
 end

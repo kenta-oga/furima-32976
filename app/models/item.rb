a78a6f509d41a637b7cf2649190c4602f32ece41
kenta-oga <kenta555phi@gmail.com>
2020-12-27 12:33:50 +0900

    モデル作成、viewファイル導入

---
+++ app/models/item.rb
@@ -0,0 +1,4 @@
+class Item < ApplicationRecord
+
+  belong_to :user
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,7 @@ class User < ApplicationRecord
     validates :nickname
     validates :birth_date
   end
+
+  has_many :items
+
 end

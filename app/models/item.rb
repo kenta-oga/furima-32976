d6343b7ecffd7c431fa5a8d97d69cf7f312aec54
kenta-oga <kenta555phi@gmail.com>
2020-12-28 11:22:12 +0900

    アクティブハッシュバリテーション設定
    モデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,18 @@
 class Item < ApplicationRecord
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping
+  belongs_to :prefecture
+  belongs_to :schedule
   belong_to :user
+
+  validates :name,          presence: true
+  validates :info,          presence: true
+  validates :category_id,   numericality: { other_than: 1 } 
+  validates :status_id,     numericality: { other_than: 1 } 
+  validates :shipping_id,   numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :schedule_id,   numericality: { other_than: 1 } 
+  validates :price,         presence: true
 end

5014ee0e602e0d18787e4243ca188a33b3731c4f
kenta-oga <kenta555phi@gmail.com>
2020-12-30 15:43:04 +0900

    before_actionにインスタンス生成をまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :destroy, :edit]
+  before_action :item_find, only: [:show, :edit, :update]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -18,16 +19,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path unless user_signed_in? && current_user.id == @item.user_id
+    redirect_to root_path if user_signed_in? && current_user.id != @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -45,3 +43,7 @@ class ItemsController < ApplicationController
                                  :price).merge(user_id: current_user.id)
   end
 end
+
+  def item_find
+    @item = Item.find(params[:id])
+  end

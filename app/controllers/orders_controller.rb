87adbfd2cdbbf32de241477ce05276eef1a30219
kenta-oga <kenta555phi@gmail.com>
2021-01-05 16:27:02 +0900

    ストロングパラメーター設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,17 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    end
+  end
 
+  private
+
+  def order_params
+    params.require(:order_address).perrmit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_num)
   end
 end
+

2f9b913124d17e20c02de1ab8817fc9b4c8f898e
kenta-oga <kenta555phi@gmail.com>
2021-01-06 22:48:20 +0900

    画面遷移設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,12 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :item_find, only: [:index, :create]
+  before_action :move_root, only: [:index, :create]
   def index
     @order_address = OrderAddress.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
@@ -28,4 +29,13 @@ class OrdersController < ApplicationController
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(amount: @item.price, card: order_params[:token], currency: 'jpy')
   end
+
+  def item_find
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_root
+    redirect_to root_path if current_user.id = @item.user_id
+  end
+
 end
